674e1f28a4f73d3c8504e4f17021dd77c2a3ecbe
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-03 13:43:04 +0300

    conf: add cjs build

--- build.js
+++ build.js
@@ -1,9 +1,22 @@
+import fs from "fs";
 import esbuild from "esbuild";
 import { nodeExternalsPlugin } from "esbuild-node-externals";
 
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+
 esbuild.build({
   entryPoints: ["./src/index.ts"],
-  outfile: "./dist/index.esm.js",
+  outfile: pkg.main,
+  format: "cjs",
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  plugins: [nodeExternalsPlugin()],
+});
+
+esbuild.build({
+  entryPoints: ["./src/index.ts"],
+  outfile: pkg.module,
   format: "esm",
   bundle: true,
   minify: true,
